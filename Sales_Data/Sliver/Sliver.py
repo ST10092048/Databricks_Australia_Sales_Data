f3381d2d671934db701e0fd3e671e2fef5bdd15c
ST10092048 <neonkgabutle81@gmail.com>
2026-02-25 07:49:42 +0000

    Update

--- Sales_Data/Sliver/Sliver.py
+++ Sales_Data/Sliver/Sliver.py
@@ -5,9 +5,15 @@
 
 # COMMAND ----------
 
-# DBTITLE 1,Customer Standardization
 from pyspark.sql.functions import col, upper, trim,lower
+# Ensure utils.logger.py exists in your workspace and is accessible
+# from utils.logger import get_logger
 
+# COMMAND ----------
+
+# DBTITLE 1,Customer Standardization
+
+# logger = get_logger("silver_pipeline")
 customer_df = spark.table("databricks_simulated_australia_sales_and_opportunities_data.v01.customers")
 customer_df = customer_df.toDF(*[c.lower() for c in customer_df.columns])
 display(customer_df.columns)
@@ -17,6 +23,7 @@ for c in string_columns:
     customer_df = customer_df.withColumn(c, lower(trim(col(c))))
 
 display(customer_df)
+# logger.info("Customer Table standardization completed")
 
 # COMMAND ----------
 
@@ -31,6 +38,7 @@ for c in string_columns:
     orders_df = orders_df.withColumn(c, lower(trim(col(c))))
 
 display(orders_df)
+# logger.info("Orders Table standardization completed")
 
 # COMMAND ----------
 
@@ -45,7 +53,26 @@ for c in string_columns:
     opportunities_df = opportunities_df.withColumn(c, lower(trim(col(c))))
 
 display(opportunities_df)
+# logger.info("Opportunities Table standardization completed")
+
+# COMMAND ----------
+
+# DBTITLE 1,Customer Null Check
+
+id_columns = ["customerid","customername"]
+customer_df_null_check = customer_df.filter(
+    sum(col(c).isNull().cast("int") for c in id_columns) > 0
+)
+
+display(customer_df_null_check)
 
 # COMMAND ----------
 
 
+
+id_columns = ["orderid","customerid","productid","orderdate"]
+orders_df_null_check = orders_df.filter(
+    sum(col(c).isNull().cast("int") for c in id_columns) > 0
+)
+
+display(orders_df_null_check)
